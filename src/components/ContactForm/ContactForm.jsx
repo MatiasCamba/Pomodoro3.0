d13e4d5b67889aab139b496b3954d0a515105954
matiascamba <matiezequielcamba@gmail.com>
2025-01-24 19:28:45 -0300

    feature/ContactForm: ContactForm ready

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,20 @@
+
+
+function ContactForm() {
+  return (
+   
+    <div>
+        <h1>Formulario</h1>
+        <form action="">
+            <label htmlFor="">Nombre</label>
+            <input type="text" />
+            <label htmlFor=""></label>
+            <input type="text" />
+            <label htmlFor=""></label>
+        </form>
+    </div>
+
+  )
+}
+
+export default ContactForm
